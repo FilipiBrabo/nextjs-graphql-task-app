162726c8bf0a21ba15fa9e3c6c005f2860bb7a68
Filipi Brabo <filipi.brabo@gmail.com>
2024-07-03 18:40:32 -0300

    fix: server i18n

--- i18n/server.ts
+++ i18n/server.ts
@@ -12,9 +12,10 @@ const initI18next = async (lng: string, ns: string | string[]) => {
   await i18nInstance
     .use(initReactI18next)
     .use(
-      resourcesToBackend((language: string, namespace: string) => {
-        import(`./locales/${language}/${namespace}.json`);
-      })
+      resourcesToBackend(
+        (language: string, namespace: string) =>
+          import(`./locales/${language}/${namespace}.json`)
+      )
     )
     .init(getOptions(lng, ns));
   return i18nInstance;
